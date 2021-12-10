6dc3bd19ca677db34a1f986e687839276cfaeaf6
Emperor_charly <charleschukwunweikeeneh@gmail.com>
2021-12-10 10:30:00 +0100

    Add JS functionalities 1. Set an array of simple tasks. 2. Add function to iterate over the tasks array and populate an HTML list item element for each task.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,62 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+// Array of some simple tasks
+const toDoTasksArray = [
+  {
+    description: 'Wake up time and breakfast',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Morning session',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Lunch break',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Lessons and project for the day',
+    completed: false,
+    index: 4,
+  },
+  {
+    description: 'Standup meeting',
+    completed: false,
+    index: 5,
+  },
+  {
+    description: 'Exercise and bath',
+    completed: false,
+    index: 6,
+  },
+  {
+    description: 'Light out and sleep',
+    completed: false,
+    index: 7,
+  }
+];
+ class ToDoList {
+  constructor(toDoTasksArray, container) {
+    this.toDoTasksArray = toDoTasksArray;
+    this.container = document.querySelector(container);
+  }
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  displayToDo() {
+    this.toDoTasksArray.sort((a, b) => a.index - b.index);
 
-  return element;
+    this.container.innerHTML = this.toDoTasksArray.map((todo) => `
+            <article>
+            <p><input type="checkbox"> ${todo.description} <span><i class="fas fa-ellipsis-v"></i></span></p>
+            <hr class="line-break">
+            </article>`).join('');
+  }
 }
 
-document.body.appendChild(component());
+const myToDo = new ToDoList(toDoTasksArray, '.list-item');
+
+document.addEventListener('DOMContentLoaded', () => {
+  myToDo.displayToDo();
+});
